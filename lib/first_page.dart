cf4f322c890a8fff7ebb34430269c8010707e376
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-22 09:29:29 +0100

    create first screen file

---
+++ lib/first_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyFirstPage extends StatelessWidget {
+  const MyFirstPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Screen n°1",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                color: Colors.teal
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(bottom: 20)),
+            RaisedButton(
+              child: const Text("Next Screen"),
+                color: Colors.teal,
+                textColor: Colors.white,
+                onPressed: null
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
