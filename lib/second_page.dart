41812a21ff8ee60f4fee4ac8d784b3f595219de9
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-22 09:31:49 +0100

    create second screen class

---
+++ lib/second_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MySecondPage extends StatelessWidget {
+  const MySecondPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Screen n°2",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                color: Colors.teal
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(bottom: 20)),
+            RaisedButton(
+              child: const Text("Previous Screen"),
+                color: Colors.teal,
+                textColor: Colors.white,
+                onPressed: null
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
